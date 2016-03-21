39f665534aae47b3e05df154a8ebed057ea330d9
Achmad Fatoni <achmadfatony@gmail.com>
2016-03-21 07:18:03 +0700

    set root view

---
+++ App/Components/Main.ios.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+    Component,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class Main extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
--- index.ios.js
+++ index.ios.js
@@ -6,26 +6,24 @@
 import React, {
   AppRegistry,
   Component,
+  NavigatorIOS,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+import Main from './App/Components/Main.ios';
+
 class Utangku extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <NavigatorIOS
+            initialRoute={{
+                component: Main,
+                title: 'Utangku',
+                rightButtonTitle: '+',
+              }}
+        />
     );
   }
 }
